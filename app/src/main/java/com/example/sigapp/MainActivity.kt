2fd44b673b43ef50776f927a3761d982c5a9f2a0
Benjamin <@p1807300>
2021-02-05 19:57:03 +0100

    metier fctel mais pas de parcours de l'itineraire

--- app/src/main/java/com/example/sigapp/MainActivity.kt
+++ app/src/main/java/com/example/sigapp/MainActivity.kt
@@ -1,90 +1,47 @@
 package com.example.sigapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.AttributeSet
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.sigapp.JsonModelClass.Distance
 import com.example.sigapp.JsonModelClass.ParcPoint
 import com.example.sigapp.JsonModelClass.ParcRoute
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    companion object {
+        private lateinit var metier : Metier
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DistanceRepository.getparcDistance().observe(this, Observer { dist ->
-            ParcRepository.getParc().observe(this, Observer {//le observe est dans le observe car il faut que les parcs soient chargés pour avoir les points
-                ParcRepository.getPoints().observe(this, Observer { points ->
-                    ParcRepository.getRoutes().observe(this, {
-                        Log.e("dijkstra", "startPoint : ${points[2].nom} et endPoint : ${points[4].nom}")
-                        dijkstra(points, it, points[2], points[4], dist.retourDistance[0])
-                    })
-                })
-            })
-            Log.e("observe dist", ""+ dist.retourDistance[0].origine)
-        })
+        metier = Metier(this)
+        Log.e("YO", "3s avant")
+        Executors.newSingleThreadScheduledExecutor().schedule({
+            Log.e("GOOOO", "Je pars de 1 à 6")
+        }, 3, TimeUnit.SECONDS)
 
 
+        /*
 
         RouteRepository.getRouteDetail(route = 1,mobile = 270).observe(this, Observer {
             findViewById<TextView>(R.id.text).text = it.retourRoute[0].code
-        })
+        })*/
     }
 
 
-    fun dijkstra(listPoint: List<ParcPoint>, listRoute: List<ParcRoute>, dep : ParcPoint, arr : ParcPoint, dist : Distance) : List<ParcRoute>{
-        var pred = emptyMap<ParcPoint, String>().toMutableMap()
-        var l = emptyMap<ParcPoint, Double>().toMutableMap()
-        var E = emptyList<ParcPoint>().toMutableList()
-
-        listPoint.forEach {
-            pred[it] = "0"
-            l[it] = 5000.0
-        }
-        l[dep] = 0.0
-        E.add(dep)
-        var s = dep
-
-        while (E.count() > 0 && s.id != arr.id){
-            s = E.first()
-            E.forEach {
-                if ( l[s]!! > l[it]!!){
-                    s = it
-                }
-            }
-            E.remove(s)
-            var myRoutes = listRoute.filter { it.pointIdDebut == s.id }
-            var mySuccesseurs = emptyList<ParcPoint>().toMutableList()
-            myRoutes.forEach { route ->
-                mySuccesseurs.add(listPoint.first { it.id == route.pointIdFin })
-            }
-            mySuccesseurs.forEach {
-                if (l[it]!! > l[s]!!+ s.getPoids(dist, it)){
-                    l[it] = l[s]!!+ s.getPoids(dist, it)
-                    pred[it] = s.id
-                    E.add(it)
-                }
-            }
-        }
-        // todo : renvoyer un truc propre a combaz
-
-        var results = emptyList<ParcRoute>().toMutableList()
-
-        var pointId = arr.id
-        while (pointId != dep.id){
-            Log.e("route", "debut : ${pointId}, fin : ${pred[listPoint.first { it.id == pointId}]}")
-            results.add(listRoute.first { route -> route.pointIdFin == pointId && route.pointIdDebut == pred[listPoint.first { it.id == pointId}] })
-            pointId = pred[listPoint.first { it.id == pointId}] ?: "0"
-        }
-        results.reversed().forEach { Log.e("route a prendre", it.routeId) }
-/*
-        listPoint.forEach {
-            Log.e("point ${it.nom}",  ": ${pred[it]}")
-        }
-*/
-        return results
-    }
 }
